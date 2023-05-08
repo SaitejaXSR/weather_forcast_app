16be65767b898f4853ed3073a92aa692c6015579
Sai teja chary <saitejachary1411@gmail.com>
2023-05-08 07:28:39 +0530

    Handle errors

--- main.py
+++ main.py
@@ -10,18 +10,20 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    filtered_data = get_data(place, days)
+    try:
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+            temperatures = [dicti["main"]["temp"] / 10 for dicti in filtered_data]
+            dates = [dicti["dt_txt"] for dicti in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    if option == "Temperature":
-        temperatures = [dicti["main"]["temp"] / 10 for dicti in filtered_data]
-        dates = [dicti["dt_txt"] for dicti in filtered_data]
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
-
-    if option == "Sky":
-        filtered_data = [dicti["weather"][0]["main"] for dicti in filtered_data]
-        images_dict = {"Clear": "sky_images/clear.png", "Clouds": "sky_images/cloud.png",
-                       "Rain": "sky_images/rain.png", "Snow": "sky_images/snow.png"}
-        st.image([images_dict[image] for image in filtered_data], width=115)
+        if option == "Sky":
+            filtered_data = [dicti["weather"][0]["main"] for dicti in filtered_data]
+            images_dict = {"Clear": "sky_images/clear.png", "Clouds": "sky_images/cloud.png",
+                           "Rain": "sky_images/rain.png", "Snow": "sky_images/snow.png"}
+            st.image([images_dict[image] for image in filtered_data], width=115)
+    except KeyError:
+        st.warning("Place does not exist.")
 
 
